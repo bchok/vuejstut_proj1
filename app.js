26ce72cd2e3590754a12bb9d79287176f696a1fd
bchok <bchok91@gmail.com>
2017-07-26 19:03:57 -0400

    implemented the attack button and damage tracker

--- app.js
+++ app.js
@@ -10,6 +10,39 @@ new Vue({
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+        },
+        attack: function(){
+            var max = 10;
+            var min = 3;
+            var damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+            this.monsterHealth -= damage;
+            
+            if(this.monsterHealth <= 0){
+                alert('You Won!');
+                this.gameIsRunning = false;
+                return;
+            }   
+
+            var mMax = 5;
+            var mMin = 12;
+            var mDamage = Math.max(Math.floor(Math.random() * max) + 1, min);
+            this.playerHealth -= mDamage;
+            
+            if(this.playerHealth <=0){
+                alert('You Lost!');
+                this.gameIsRunning = false;
+            }
+        },
+        specialAttack: function(){
+
+        },
+        heal: function(){
+
+        },
+        giveUp: function(){
+            this.gameIsRunning = false;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
         }
     }
 
