63f005872c87b7acde54ea223e915d0b9da4676b
bchok <bchok91@gmail.com>
2017-07-26 21:16:00 -0400

    implemented basic logic for special attacks and heals

--- app.js
+++ app.js
@@ -23,10 +23,18 @@ new Vue({
             this.checkWin();    
         },
         specialAttack: function(){
+            this.monsterHealth -= this.calculateDamage(6, 20);
 
+            if (this.checkWin()){
+                return;
+            }
+
+            this.playerHealth -= this.calculateDamage(5, 12);
+
+            this.checkWin();
         },
         heal: function(){
-
+            this.playerHealth += Math.max(Math.floor(Math.random() * 15) + 1, 5);
         },
         giveUp: function(){
             this.gameIsRunning = false;
